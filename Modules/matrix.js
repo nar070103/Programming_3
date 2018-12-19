5de3b38aca27e65e20b3dadc131ced25d30ebfe1
user <user@lab0000m.tumo.lab>
2018-12-19 21:28:27 +0400

    _Rima project

--- Modules/matrix.js
+++ Modules/matrix.js
@@ -1,4 +1,7 @@
-
+var Grass = require("./class.grass");
+var GrassEater = require("./class.grassEater");
+var Predator = require("./class.Predator");
+var Ell = require("./class.Ell");
 var m = 80;
 var n = 80;
 var matrix = [];
@@ -16,3 +19,62 @@ for (var y = 0; y < m; y++) {
         console.log(random_item(items));
     }
 }
+
+
+
+
+var Grass = require("./class.grass");
+var GrassEater = require("./class.grasseater");
+var Predator = require("./class.predator");
+var Fish = require("./class.fish");
+
+var matrix = [];
+var n = 50;
+var m = 50;
+
+var arr = [];
+var dzukQanak = 25;
+
+for (var y = 0; y < n; y++) {
+    matrix[y] = [];
+    for (var x = 0; x < m; x++) {
+        matrix[y][x] = random([0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 3]); //
+        if (x + y >= n - 4 && x + y <= n + 2) {
+            matrix[y][x] = 4;
+            arr.push([x, y]);
+        }
+    }
+}
+
+function random(arr)
+{
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+while (dzukQanak > 0) {
+    var kord = random(arr);
+    //console.log(kord);
+    var x = kord[0];
+    var y = kord[1];
+    matrix[y][x] = 5;
+    dzukQanak--;
+}
+
+for (var y = 0; y < matrix.length; y++) {
+    for (var x = 0; x < matrix[y].length; x++) {
+        if (matrix[y][x] == 1) {
+            matrix[y][x] = new Grass(x, y, 1);
+        }
+        if (matrix[y][x] == 2) {
+            matrix[y][x] = new GrassEater(x, y, 2);
+        }
+        if (matrix[y][x] == 3) {
+            matrix[y][x] = new Predator(x, y, 3);
+        }
+        if (matrix[y][x] == 5) {
+            matrix[y][x] = new Fish(x, y, 5);
+        }
+    }
+}
+
+module.exports = matrix;
--- app.js
+++ app.js
@@ -2,60 +2,47 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var Grass = require('./Modules/class.grass.js');
+var matrix = require('./Modules/matrix.js')
 
 app.use(express.static("."));
+
 app.get('/', function (req, res) {
   res.redirect('index.html');
 });
-server.listen(3000);
 
-var Grass = require("./modules/class.grass.js");
-var GrassEater = require("./modules/class.grassEater");
-var Predator = require("./modules/class.Predator");
-var Ell = require("./modules/class.Ell");
-var LivingCreature = require("./modules/class.LivingCreature");
+app.listen(3000);
 
-var time = frameRate(5);
+io.on('connection', function (socket) {
+  socket.emit("send matrix", matrix);
 
-function frameRate(frameCount) {
+  setInterval(function(){  
+    for (var y = 0; y < matrix.length; y++) {
+      for (var x = 0; x < matrix[y].length; x++) {
+          if (matrix[y][x].index == 1) {
+              matrix[y][x].mul(matrix);
+          }
+          if (matrix[y][x].index == 2) {
+              matrix[y][x].eat(matrix);
+          }
+          if (matrix[y][x].index == 3) {
+              matrix[y][x].eat(matrix);
+          }
+          if (matrix[y][x].index == 4) {
+              matrix[y][x].eat1(matrix);
+          }
+          if (matrix[y][x].index == 5) {
+              matrix[y][x].eat(matrix);
+          }
+      }
+  }
+
+    socket.emit("redraw", matrix);
+  }, time);
+});
+
+
+var time = frameRate(1);
+function frameRate(frameCount){
   return 1000 / frameCount;
 }
-
-function draw() {
-  for (var y = 0; y < matrix.length; y++) {
-    for (var x = 0; x < matrix[y].length; x++) {
-        if (matrix[y][x] == 1) {
-            matrix[y][x] = new Grass(x, y, 1);
-        }
-        else if (matrix[y][x] == 2) {
-            matrix[y][x] = new GrassEater(x, y, 2);
-        }
-        else if (matrix[y][x] == 3) {
-            matrix[y][x] = new Predator(x, y, 3);
-        }
-        else if (matrix[y][x] == 4) {
-            matrix[y][x] = new Ell(x, y, 4);
-        }
-    }
-}
-}
-  background("#acacac");
-    for (var y = 0; y < matrix.length; y++) {
-        for (var x = 0; x < matrix[y].length; x++) {
-            if (matrix[y][x].index == 1) {
-                matrix[y][x].mul();
-            }
-            else if (matrix[y][x].index == 2) {
-                matrix[y][x].eat();
-            }
-            else if (matrix[y][x].index == 3) {
-                matrix[y][x].eat();
-            }
-            else if (matrix[y][x].index == 4) {
-                matrix[y][x].eat1();
-            }
-        }
-    
-}
-
-setInterval(draw, time)
